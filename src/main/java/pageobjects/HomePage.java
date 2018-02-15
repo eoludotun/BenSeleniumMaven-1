0c3cdf107f9a6f50c835f76deee69979113cb809
eoludotun <eoludotun@users.noreply.github.com>
2018-02-15 05:44:07 -0500

    Update HomePage.java

--- src/main/java/pageobjects/HomePage.java
+++ src/main/java/pageobjects/HomePage.java
@@ -3,9 +3,6 @@ package pageobjects;
 import org.openqa.selenium.WebDriver;
 import support.Driver;
 
-/**
- * Created by zsuleiman on 04/02/2017.
- */
 public class HomePage extends Driver{
 
     public static final String HOME_PAGE = "https://www.amazon.co.uk";
